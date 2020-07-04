a7faf90b4f49eaac381cd9b6791ec21c742c2764
Peter Pan <peter@never.lan>
2020-07-04 08:40:37 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-81a053a90f971ce1623a.js"
+    "url": "webpack-runtime-39a7d409919435dbb254.js"
   },
   {
     "url": "framework-95b680cb3190aa9bfe59.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6f244fc3665cb65365b378fbf6a94387"
+    "revision": "e5f16f0ddc7ab129acbe6231b0f66fbb"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0097d26fbd474b34ff9b.js"
